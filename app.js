5364eb0740e1baf32b78609cdd892aa9fcd665b4
Ali-johnno <ajay051701@gmail.com>
2020-11-10 21:14:10 -0500

    Changes in variable names and a minor changes

--- app.js
+++ app.js
@@ -5,32 +5,31 @@ document.addEventListener("DOMContentLoaded", function(){
     const superheroAlias = document.getElementById('alias');
     const superheroName = document.getElementById('name');
     const superheroBio = document.getElementById('biography');
-    const noSuperhero = document.getElementById('notFound');
     const httpRequest = new XMLHttpRequest();
 
     searchButton.addEventListener("click", function(e){
         e.preventDefault();
         var searchRequest = document.getElementById('searchBox').value;
-        noSuperhero.innerHTML = '';
         superheroAlias.innerHTML = '';
         superheroName.innerHTML = '';
         superheroBio.innerHTML = '';
         result.innerHTML = '';
+        result.classList.remove("notFound");
 
         httpRequest.onreadystatechange = function(){
             if (httpRequest.readyState == 4){
                 if (httpRequest.status == 200){
                     if (searchRequest.length !== 0){
-                        var idk = httpRequest.responseText;
-                        var ting = JSON.parse(idk);
-                        if (ting == "Superhero not found"){
-                            noSuperhero.innerHTML= ting;
-                            noSuperhero.classList.add("notFound");
+                        var searchResults = httpRequest.responseText;
+                        var parseResults = JSON.parse(searchResults);
+                        if (parseResults == "Superhero not found"){
+                            result.innerHTML= parseResults;
+                            result.classList.add("notFound");
                         } 
                         else {
-                            superheroAlias.innerHTML =ting.alias.toUpperCase();
-                            superheroName.innerHTML = "A.K.A " + ting.name.toUpperCase();
-                            superheroBio.innerHTML = ting.biography;
+                            superheroAlias.innerHTML =parseResults.alias.toUpperCase();
+                            superheroName.innerHTML = "A.K.A " + parseResults.name.toUpperCase();
+                            superheroBio.innerHTML = parseResults.biography;
                         }  
                    } else {
                         result.innerHTML = httpRequest.responseText;
